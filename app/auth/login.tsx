9cf976693f5535bcdb387a4ff45d983493186493
ashishsahario04@gmail.com <ashishsahario04@gmail.com>
2025-03-29 09:05:49 +0530

    done with auth, working on both web and android

--- app/auth/login.tsx
+++ app/auth/login.tsx
@@ -1,139 +1,139 @@
-import { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Button, Alert, Platform } from 'react-native';
-import * as AuthSession from 'expo-auth-session';
-import * as WebBrowser from 'expo-web-browser';
-import {jwtDecode} from 'jwt-decode';
-import { router } from 'expo-router';
+// import { useState, useEffect } from 'react';
+// import { StyleSheet, Text, View, Button, Alert, Platform } from 'react-native';
+// import * as AuthSession from 'expo-auth-session';
+// import * as WebBrowser from 'expo-web-browser';
+// import {jwtDecode} from 'jwt-decode';
+// import { router } from 'expo-router';
 
-WebBrowser.maybeCompleteAuthSession();
+// WebBrowser.maybeCompleteAuthSession();
 
-let redirectUri = AuthSession.makeRedirectUri({path:"/home/homePage"});
-let CLIENT_SECRET = "w7AnnMlXO8w6JUX1zyBMoXP5A8TKuj2nmhqwM5rsr10a"
-let CLIENT_ID = "EEkeq5vwFe2Ue0Ieca5anfXx7JQa";
-if(Platform.OS == "android")
-{
-  redirectUri = AuthSession.makeRedirectUri();
-  CLIENT_ID = "EEkeq5vwFe2Ue0Ieca5anfXx7JQa"
-}
-else if(Platform.OS == "web")
-{
-  CLIENT_ID = "Mj4BD1X2Rv3reU48BsHuxLV0aLka"
-}
-
-
-export default function App() {
-
-    const discovery = AuthSession.useAutoDiscovery('https://api.asgardeo.io/t/genmedx/oauth2/token');
-    const [tokenResponse, setTokenResponse] = useState({});
-    const [decodedIdToken, setDecodedIdToken] = useState({});
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-    const [request, result, promptAsync] = AuthSession.useAuthRequest(
-        {
-            redirectUri,
-            clientId: CLIENT_ID,
-            responseType: "code",
-            scopes: ["openid", "profile", "email", "phone"]
-        },
-        discovery
-    );
-
-    const getAccessToken = () => {
-      if (result?.type == "success") {
-
-        let tempHeader = {};
-        const authHeader = "Basic " + btoa(`${CLIENT_ID}:${CLIENT_SECRET}`);
+// let redirectUri = AuthSession.makeRedirectUri({path:"/home/homePage"});
+// let CLIENT_SECRET = "w7AnnMlXO8w6JUX1zyBMoXP5A8TKuj2nmhqwM5rsr10a"
+// let CLIENT_ID = "EEkeq5vwFe2Ue0Ieca5anfXx7JQa";
+// if(Platform.OS == "android")
+// {
+//   redirectUri = AuthSession.makeRedirectUri();
+//   CLIENT_ID = "EEkeq5vwFe2Ue0Ieca5anfXx7JQa"
+// }
+// else if(Platform.OS == "web")
+// {
+//   CLIENT_ID = "Mj4BD1X2Rv3reU48BsHuxLV0aLka"
+// }
 
 
-        if(Platform.OS == "web")
-        {
-          tempHeader = {
-            "Content-Type": "application/x-www-form-urlencoded",
-              "Authorization": authHeader, 
-          }
-        }
-        else if(Platform.OS == "android")
-        {
-          tempHeader = {
-            "Content-Type" : "application/x-www-form-urlencoded",
-          }
-        }
+// export default function App() {
 
-        fetch(
-        "https://api.asgardeo.io/t/genmedx/oauth2/token",
-          {
-            method: "POST",
-            headers: tempHeader,
-            body: `grant_type=authorization_code&code=${result?.params?.code}&redirect_uri=${redirectUri}&client_id=${CLIENT_ID}&code_verifier=${request?.codeVerifier}`
-          }).then((response) => {
-              return response.json();
-            }).then((data) => {
-              setTokenResponse(data);
-              setDecodedIdToken(jwtDecode(data.id_token));
-              console.log(jwtDecode(data.id_token));
-              // console.log(data);
-              setIsAuthenticated(true);
-            }).catch((err) => {
-              console.log(err);
-            });
-        }
-    }
+//     const discovery = AuthSession.useAutoDiscovery('https://api.asgardeo.io/t/genmedx/oauth2/token');
+//     const [tokenResponse, setTokenResponse] = useState({});
+//     const [decodedIdToken, setDecodedIdToken] = useState({});
+//     const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-    useEffect(() => {
-      (async function setResult() {
-        if (result) {
-          if (result.type == "error") {
-            Alert.alert(
-              "Authentication error",
-              result.params.error_description || "something went wrong"
-            );
-            return;
-          }
-          if (result.type === "success") {
-            try{
+//     const [request, result, promptAsync] = AuthSession.useAuthRequest(
+//         {
+//             redirectUri,
+//             clientId: CLIENT_ID,
+//             responseType: "code",
+//             scopes: ["openid", "profile", "email", "phone"]
+//         },
+//         discovery
+//     );
 
-              await getAccessToken();
-            }
-            catch(err)
-            {
-              console.log(err);
-            }
+//     const getAccessToken = () => {
+//       if (result?.type == "success") {
+
+//         let tempHeader = {};
+//         const authHeader = "Basic " + btoa(`${CLIENT_ID}:${CLIENT_SECRET}`);
+
+
+//         if(Platform.OS == "web")
+//         {
+//           tempHeader = {
+//             "Content-Type": "application/x-www-form-urlencoded",
+//               "Authorization": authHeader, 
+//           }
+//         }
+//         else if(Platform.OS == "android")
+//         {
+//           tempHeader = {
+//             "Content-Type" : "application/x-www-form-urlencoded",
+//           }
+//         }
+
+//         fetch(
+//         "https://api.asgardeo.io/t/genmedx/oauth2/token",
+//           {
+//             method: "POST",
+//             headers: tempHeader,
+//             body: `grant_type=authorization_code&code=${result?.params?.code}&redirect_uri=${redirectUri}&client_id=${CLIENT_ID}&code_verifier=${request?.codeVerifier}`
+//           }).then((response) => {
+//               return response.json();
+//             }).then((data) => {
+//               setTokenResponse(data);
+//               setDecodedIdToken(jwtDecode(data.id_token));
+//               console.log(jwtDecode(data.id_token));
+//               // console.log(data);
+//               setIsAuthenticated(true);
+//             }).catch((err) => {
+//               console.log(err);
+//             });
+//         }
+//     }
+
+//     useEffect(() => {
+//       (async function setResult() {
+//         if (result) {
+//           if (result.type == "error") {
+//             Alert.alert(
+//               "Authentication error",
+//               result.params.error_description || "something went wrong"
+//             );
+//             return;
+//           }
+//           if (result.type === "success") {
+//             try{
+
+//               await getAccessToken();
+//             }
+//             catch(err)
+//             {
+//               console.log(err);
+//             }
             
-          }
-        }
-      })();
-    }, [result]);
+//           }
+//         }
+//       })();
+//     }, [result]);
     
-    if(isAuthenticated)
-      {
+//     if(isAuthenticated)
+//       {
         
-        router.replace({
-                      pathname: "/home/homePage",
-                      params: { user: JSON.stringify(decodedIdToken) }
-                    });
+//         router.replace({
+//                       pathname: "/home/homePage",
+//                       params: { user: JSON.stringify(decodedIdToken) }
+//                     });
         
-      }
+//       }
       
 
-    return (
-      <View style={styles.container}>
-        <Button title="Login" disabled={!request} onPress={() => promptAsync()} />
+//     return (
+//       <View style={styles.container}>
+//         <Button title="Login" disabled={!request} onPress={() => promptAsync()} />
         
-      </View>
-    );
-}
+//       </View>
+//     );
+// }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    accessTokenBlock: {
-        width: 300,
-        height: 500,
-        overflow: "scroll"
-    }
-});
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         backgroundColor: '#fff',
+//         alignItems: 'center',
+//         justifyContent: 'center',
+//     },
+//     accessTokenBlock: {
+//         width: 300,
+//         height: 500,
+//         overflow: "scroll"
+//     }
+// });
 
--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,117 @@
-import { Text, View, StyleSheet, ImageBackground, TouchableOpacity } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  ImageBackground,
+  TouchableOpacity,
+  Alert,
+  Platform,
+} from "react-native";
 import { Link } from "expo-router";
 import { useRouter } from "expo-router";
 import { Button } from "@react-navigation/elements";
+import { useState, useEffect } from "react";
+// import { StyleSheet, Text, View, Button, Alert, Platform } from 'react-native';
+import * as AuthSession from "expo-auth-session";
+import * as WebBrowser from "expo-web-browser";
+import { jwtDecode } from "jwt-decode";
+import { router } from "expo-router";
 
+WebBrowser.maybeCompleteAuthSession();
+
+let redirectUri = AuthSession.makeRedirectUri({ path: "/home/homePage" });
+let CLIENT_SECRET = "w7AnnMlXO8w6JUX1zyBMoXP5A8TKuj2nmhqwM5rsr10a";
+let CLIENT_ID = "EEkeq5vwFe2Ue0Ieca5anfXx7JQa";
+if (Platform.OS == "android") {
+  redirectUri = AuthSession.makeRedirectUri();
+  CLIENT_ID = "EEkeq5vwFe2Ue0Ieca5anfXx7JQa";
+} else if (Platform.OS == "web") {
+  CLIENT_ID = "Mj4BD1X2Rv3reU48BsHuxLV0aLka";
+}
 
 export default function Index() {
+  const discovery = AuthSession.useAutoDiscovery(
+    "https://api.asgardeo.io/t/genmedx/oauth2/token"
+  );
+  const [tokenResponse, setTokenResponse] = useState({});
+  const [decodedIdToken, setDecodedIdToken] = useState({});
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  const router = useRouter();
+  const [request, result, promptAsync] = AuthSession.useAuthRequest(
+    {
+      redirectUri,
+      clientId: CLIENT_ID,
+      responseType: "code",
+      scopes: ["openid", "profile", "email", "phone"],
+    },
+    discovery
+  );
+
+  const getAccessToken = () => {
+    if (result?.type == "success") {
+      let tempHeader = {};
+      const authHeader = "Basic " + btoa(`${CLIENT_ID}:${CLIENT_SECRET}`);
+
+      if (Platform.OS == "web") {
+        tempHeader = {
+          "Content-Type": "application/x-www-form-urlencoded",
+          Authorization: authHeader,
+        };
+      } else if (Platform.OS == "android") {
+        tempHeader = {
+          "Content-Type": "application/x-www-form-urlencoded",
+        };
+      }
+
+      fetch("https://api.asgardeo.io/t/genmedx/oauth2/token", {
+        method: "POST",
+        headers: tempHeader,
+        body: `grant_type=authorization_code&code=${result?.params?.code}&redirect_uri=${redirectUri}&client_id=${CLIENT_ID}&code_verifier=${request?.codeVerifier}`,
+      })
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          setTokenResponse(data);
+          setDecodedIdToken(jwtDecode(data.id_token));
+          console.log(jwtDecode(data.id_token));
+          // console.log(data);
+          setIsAuthenticated(true);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+
+  useEffect(() => {
+    (async function setResult() {
+      if (result) {
+        if (result.type == "error") {
+          Alert.alert(
+            "Authentication error",
+            result.params.error_description || "something went wrong"
+          );
+          return;
+        }
+        if (result.type === "success") {
+          try {
+            await getAccessToken();
+          } catch (err) {
+            console.log(err);
+          }
+        }
+      }
+    })();
+  }, [result]);
+
+  if (isAuthenticated) {
+    router.replace({
+      pathname: "/home/homePage",
+      params: { user: JSON.stringify(decodedIdToken) },
+    });
+  }
+
   return (
     <ImageBackground
       source={require("../assets/images/landingPageBackground.jpg")}
@@ -18,14 +123,18 @@ export default function Index() {
           Welcome to <Text style={styles.companyName}>GenmedX</Text>
         </Text>
         <Text style={styles.subtitle}>
-          Empower your health journey with innovative AI insights that guide you toward a vibrant, healthier life. Experience the future of personalized wellness.
+          Empower your health journey with innovative AI insights that guide you
+          toward a vibrant, healthier life. Experience the future of
+          personalized wellness.
         </Text>
 
-        <TouchableOpacity style= {styles.exploreButton} onPressOut={() => router.push("/auth/login")}>
-            <Text style={styles.exploreButtonText}>Explore </Text>
-            
-          </TouchableOpacity>
-          {/* <Button >         </Button> */}
+        <TouchableOpacity
+          style={styles.exploreButton}
+          onPressOut={() => promptAsync()}
+        >
+          <Text style={styles.exploreButtonText}>Explore </Text>
+        </TouchableOpacity>
+        {/* <Button >         </Button> */}
       </View>
     </ImageBackground>
   );
@@ -54,7 +163,7 @@ const styles = StyleSheet.create({
     fontFamily: "sans-serif",
   },
   subtitle: {
-    minWidth :"70%",  
+    minWidth: "70%",
     fontSize: 18,
     textAlign: "center",
     color: "#34495e",
@@ -75,7 +184,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 40,
     borderRadius: 25,
     borderWidth: 2,
-    borderColor :"white",
+    borderColor: "white",
     // shadowColor:"blue"
     // borderColor: "#f53d00", // Same as company name color
   },
